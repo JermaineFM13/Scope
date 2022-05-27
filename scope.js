f838db81f893338b10c6b255c9bc6e7bea02cc86
JermaineFM13 <105619709+JermaineFM13@users.noreply.github.com>
2022-05-27 15:31:40 +1200

    Add files via upload

---
+++ scope.js
@@ -0,0 +1,28 @@
+// Variable Scope allows us to know where and when our declared variables remain valid and can be used
+// A Code Block is a code within these --> {}
+// BOM Browser Object Model
+//eg if {}, for{} function(){}.
+// let a = 10;
+// console.log("0", a); //will retun 10
+// function house() {
+//   a = 20;
+//   console.log("1", a); //Will return 20
+// }
+// console.log("2", a); // Will return 10
+// house();
+// console.log("3", a); //Will return 20 because "a" has been redefined
+
+
+//Exercise 1
+// let myName = "Jermaine";
+// function sayHi() {
+//   let myName = "Jayda";
+//   console.log(myName + " says good morning.");
+
+// }
+// function sayHi2() {
+//   console.log(myName + " says good morning again");
+// }
+// sayHi();
+// sayHi2();
+
